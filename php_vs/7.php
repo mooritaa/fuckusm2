e095dd5fb9c0172ea38997ba2a8d419858f198d3
m0ra-g <88517583+m0ra-g@users.noreply.github.com>
2024-10-04 17:31:22 -0400

    04/10/24 7.php

--- php_vs/7.php
+++ php_vs/7.php
@@ -1,53 +1,33 @@
-<html> 
-<head> 
-  <title>Mi album de fotografias</title> 
-</head> 
- 
-<body> 
-<h1> Mi album de fotos en linea </h1> 
-<h3> Cargar archivo </h3> 
- 
-<form action='<?php echo $_SERVER['PHP_SELF'] ?>' method="post" enctype="multipart/form-data"> 
-<input type="file" name="archivo"> 
-<input type="submit" name="submit" value="Cargar archivo"> 
-</form> 
- 
-<?php 
-//conectado al 33
-$ruta="img/"; // Indicar ruta 
-if (isset($_FILES['archivo']) && $_FILES['archivo']['size'] > 0) { 
-   $tamanyomax = 200000; // Indicar tamaño en bytes 
-   $nombretemp = $_FILES['archivo']['tmp_name']; 
-   $nombrearchivo = $_FILES['archivo']['name']; 
-   $tamanyoarchivo = $_FILES['archivo']['size']; 
-   $tipoarchivo = GetImageSize( $nombretemp ); 
-   if ($tipoarchivo[2] == 1 || $tipoarchivo[2] == 2) { //Gif o Jpg? 
-       if ($tamanyoarchivo <= $tamanyomax) { // Archivo demasiado grande? 
-          if (move_uploaded_file($nombretemp, $ruta.$nombrearchivo)) { 
-      echo "<p>El archivo se ha cargado <b>con exito</b>. 
-      Tamaño de archivo: <b>$tamanyoarchivo</b> bytes, 
-      Nombre de imagen: <b>$nombrearchivo</b><br></p>"; 
-   } else { 
-             echo "<p>No se ha podido cargar el archivo.</p>";
-            } 
-        } else { 
-              echo "<p>El archivo tiene mas de <b>$tamanyomax bytes</b> y es demasiado grande.</p>"; 
-        } 
-     } else { 
-       echo "<p>No es un archivo GIF o JPG valido.</p>"; 
-     } 
-     echo "<form action='{$_SERVER['PHP_SELF']}' method='post'> 
-     <input type='submit' value='OK'></form>"; 
- } 
- $filehandle = opendir($ruta); // Abrir archivos 
- while ($file=readdir($filehandle)) { 
- if ($file != "." && $file != "..") { 
-    $tamanyo = GetImageSize($ruta.$file); 
-    echo "<p><img src='$ruta$file' $tamanyo[3]><br></p>\n"; 
- } 
- } 
- closedir($filehandle);  // Fin lectura de archivos 
- ?> 
-  
- </body> 
- </html>
+<?php
+   require "functions.php";
+   $respuesta="";
+   if(isset($_FILES['files'])){
+      $respuesta = uploadFiles($_FILES);
+
+   }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>album</title>
+</head>
+<body>
+   
+   <form action="" method="post" enctype="multipart/form-data">
+      <h1>Elegir imagen</h1>
+      <input type="file" name="files[]" multiple>
+      <button type="submit" name="enviar">enviar</button>
+      <?php
+         if($respuesta == "bien"){
+            echo "<p class=bien> archivo subido!! </p>";
+         } else{
+            echo "<p class=error> $respuesta </p>";
+         }
+      ?>
+   </form>
+
+</body>
+</html>
