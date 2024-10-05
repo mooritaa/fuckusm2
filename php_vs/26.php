7eec5fd126c49b39e16bc4b480c358edb5f6ea4c
m0ra-g <88517583+m0ra-g@users.noreply.github.com>
2024-10-05 16:37:22 -0400

    Add files via upload

---
+++ php_vs/26.php
@@ -0,0 +1,19 @@
+<html> 
+<head> 
+<title>Problema</title> 
+</head> 
+<body> 
+<?php 
+if (isset($_COOKIE['usuario'])){ 
+echo "Cookie de sesión creada. Su valor es $_COOKIE[usuario]";} 
+else{ 
+echo "No existe cookie de sesión";}
+?> 
+<br> 
+<a href="27.php">Crear cookie de sesión</a> 
+</body> 
+</html>
+
+
+
+ 
