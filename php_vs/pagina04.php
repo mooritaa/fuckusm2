d826aee5ee163185ee6605301fb5b93e3928ef5e
Pun-chi <emilychaparro06@gmail.com>
2024-10-05 17:07:51 -0400

    pagina04.php

--- php_vs/pagina04.php
+++ php_vs/pagina04.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "phpfacil1";
+$dbname = "phpfacil";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
