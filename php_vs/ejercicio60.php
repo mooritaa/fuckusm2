83ffeff89aac7dfd18d580cb985a85c22233b704
adrirod04 <adrimarcano2004@gmail.com>
2024-10-06 16:18:00 -0400

    Add files via upload

---
+++ php_vs/ejercicio60.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Problema</title>
+</head>
+<body>
+    
+    <form action="pagina60.php" method="post">
+        Ingrese nombre de usuario:
+        <input type="text" name="campousuario"><br>
+        Ingrese clave:
+        <input type="password" name="campoclave"><br>
+        <input type="submit" value="confirmar">
+    </form>
+</body>
+</html>
